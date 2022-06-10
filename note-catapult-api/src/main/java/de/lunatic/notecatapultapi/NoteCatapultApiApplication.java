ae978d630b528bc6161af5d77e3698d6efb35811
phlipphlip <philip@lunatic-circus.de>
2022-06-10 23:06:50 +0000

    init

---
+++ note-catapult-api/src/main/java/de/lunatic/notecatapultapi/NoteCatapultApiApplication.java
@@ -0,0 +1,13 @@
+package de.lunatic.notecatapultapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCatapultApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteCatapultApiApplication.class, args);
+    }
+
+}
